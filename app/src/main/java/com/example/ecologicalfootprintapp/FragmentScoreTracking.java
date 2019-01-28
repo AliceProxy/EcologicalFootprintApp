fc2b0ea2c5f63b3a45996e948b8d66a10795976b
waskod <waskod@oregonstate.edu>
2019-01-27 22:56:07 -0800

    Completed:  - Buttons within nav menu working and have fragment loading

---
+++ app/src/main/java/com/example/ecologicalfootprintapp/FragmentScoreTracking.java
@@ -0,0 +1,25 @@
+package com.example.ecologicalfootprintapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentScoreTracking extends Fragment {
+    private static final String TAG = "FragmentTracking";
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_tracking_layout, container, false);
+        Log.d(TAG, "onCreateView: started.");
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/ecologicalfootprintapp/MainActivity.java
+++ app/src/main/java/com/example/ecologicalfootprintapp/MainActivity.java
@@ -1,51 +1,134 @@
 package com.example.ecologicalfootprintapp;
 
-
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+{
 
-    private DrawerLayout myDrawerLayout;
-    private ActionBarDrawerToggle myToggle;
+    private DrawerLayout navDrawer;
+    private ActionBarDrawerToggle navToggle;
+    private ActionBar actionBar;
 
-    private Toolbar myToolBar;
+    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myToolBar = (Toolbar) findViewById(R.id.nav_action);
-        setSupportActionBar(myToolBar);
+        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        mViewPager = findViewById(R.id.fragmentContainer);
+        // setting up the pager
+        setupViewPager(mViewPager);
 
+        navDrawer = (DrawerLayout) findViewById(R.id.navDrawerLayout);
+        navToggle = new ActionBarDrawerToggle(this, navDrawer, R.string.open, R.string.close);
+        navDrawer.addDrawerListener(navToggle);
+        navDrawer.setBackgroundColor(Color.parseColor("#008577"));
+        navToggle.syncState();
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00574B")));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
-        //NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
-        //navigationView.setNavigationItemSelectedListener(this);
+    private void setupViewPager(ViewPager viewPager)
+    {
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FragmentHome(), "FragmentHome");
+        adapter.addFragment(new FragmentScoreBreakdown(), "FragmentBreakdown");
+        adapter.addFragment(new FragmentScoreTracking(), "FragmentTracking");
+        adapter.addFragment(new FragmentReduceImpact(), "FragmentImpact");
+        adapter.addFragment(new FragmentCorvallisResources(), "FragmentCorvallis");
+        adapter.addFragment(new FragmentAbout(), "FragmentAbout");
+;
 
-        myDrawerLayout = (DrawerLayout) findViewById(R.id.myDrawer);
-        myToggle = new ActionBarDrawerToggle(this, myDrawerLayout, R.string.open, R.string.close);
-        myDrawerLayout.addDrawerListener(myToggle);
-        myToggle.syncState();
+        // copy and repeat this line for all fragments, first fragment will be loaded at start
+        viewPager.setAdapter(adapter);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void setViewPager(int fragmentNumber)
+    {
+        mViewPager.setCurrentItem(fragmentNumber);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(myToggle.onOptionsItemSelected(item))
-        {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(navToggle.onOptionsItemSelected(item))
             return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        Intent startNewActivity;
 
+        if(id == R.id.home)
+        {
+            Toast.makeText(this,"Clicked Home", Toast.LENGTH_SHORT).show();
+            mViewPager.setCurrentItem(0);
+            navDrawer.closeDrawers();
+        }
+        if(id == R.id.scoreBreakdown)
+        {
+            Toast.makeText(this, "Clicked Score Breakdown", Toast.LENGTH_SHORT).show();
+            mViewPager.setCurrentItem(1);
+            navDrawer.closeDrawers();
+
+        }
+        if(id == R.id.scoreTracking)
+        {
+            Toast.makeText(this,"Clicked Score Tracking", Toast.LENGTH_SHORT).show();
+            // for moving to a new activity
+            //startNewActivity = new Intent(this, ScoreTracking.class);
+            // startNewActivity.putExtra(EXTRA_MESSAGE, message);
+            //startActivity(startNewActivity);
+            mViewPager.setCurrentItem(2);
+            navDrawer.closeDrawers();
+        }
+        if(id == R.id.reduceImpact)
+        {
+            Toast.makeText(this,"Clicked Reduce Impact", Toast.LENGTH_SHORT).show();
+            mViewPager.setCurrentItem(3);
+            navDrawer.closeDrawers();
+
+        }
+        if(id == R.id.corvallisResources)
+        {
+            Toast.makeText(this,"Clicked Corvallis Resources", Toast.LENGTH_SHORT).show();
+            mViewPager.setCurrentItem(4);
+            navDrawer.closeDrawers();
+
+        }
+        if(id == R.id.about)
+        {
+            Toast.makeText(this,"Clicked About", Toast.LENGTH_SHORT).show();
+            mViewPager.setCurrentItem(5);
+            navDrawer.closeDrawers();
+
+        }
+
+        return false;
+    }
 }
