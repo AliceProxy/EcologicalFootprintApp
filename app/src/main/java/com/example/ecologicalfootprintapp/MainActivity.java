0bbb5503d1cdc3f1ebb1086034d283b43f35e898
waskod <waskod@oregonstate.edu>
2019-03-17 21:45:04 -0700

    merging

--- app/src/main/java/com/example/ecologicalfootprintapp/MainActivity.java
+++ app/src/main/java/com/example/ecologicalfootprintapp/MainActivity.java
@@ -78,16 +78,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         Log.d(TAG, "questionaire completed: "+(questionaire.getCompleted()+""));
         Log.d(TAG, "questionaire completed");
-
-<<<<<<< HEAD
-
-
-
-
-
-=======
+        
         FetchAPI();
->>>>>>> 7d971f45f8725bd4cbb9f24db65078d4f590f859
+
     }
 
 
